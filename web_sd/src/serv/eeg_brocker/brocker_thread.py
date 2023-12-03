6e0b32a2a52b325aad7e033198bb43f4f583a43f
Leszek <rhoninn11@gmail.com>
2023-12-03 19:40:19 +0100

    - egg server added

--- web_sd/src/serv/eeg_brocker/brocker_thread.py
+++ web_sd/src/serv/eeg_brocker/brocker_thread.py
@@ -1,4 +1,5 @@
 
+import math
 import time
 
 from core.utils.utils_thread import ThreadWrap
@@ -7,6 +8,9 @@ from core.utils.utils_thread import ThreadWrap
 class brocker_logic_thread(ThreadWrap):
     def __init__(self):
         ThreadWrap.__init__(self)
+        self.last_data_gen_tp = time.time()
+        fps = 30
+        self.ttreshold = 1/fps
 
     def process_request(self, request):
         si = self.script_index
@@ -15,11 +19,28 @@ class brocker_logic_thread(ThreadWrap):
 
         return
     
-    def generate_eeg_data(self):
-        generate_next_data = False
+    def data_gen(self):
+        data_list = []
+        samples = 10
+        f = 1 #Hz
+        for i in range(samples):
+            off = i/(samples-1) + self.last_data_gen_tp
+            sv = math.sin(2*3.14*f+off)
+            data_list.append(sv)
+
+        return data_list
+
+
+    
+    def control_data_gen(self):
+        now = time.time()
+        generate_next_data = now - self.last_data_gen_tp > self.ttreshold
         progress = 0
         if generate_next_data:
-            self.out_queue.queue_item("eeg_data")
+            self.last_data_gen_tp += self.ttreshold
+            new_data = self.data_gen()
+            self.out_queue.queue_item(new_data)
+            # print(f"+++ eeg data generated")
             progress += 1
 
         return progress
@@ -29,9 +50,9 @@ class brocker_logic_thread(ThreadWrap):
         print(f"+++ stable diffusion thread ready")
         in_queue = self.in_queue
         while self.run_cond:
-            progress = self.generate_eeg_data()
+            progress = self.control_data_gen()
             if progress == 0:
-                time.sleep(0.1)
+                time.sleep(0.01)
 
 
 
